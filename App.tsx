937731da78d86eec192b1082cd314c5032c754bd
Josep Núñez Riba <Pepito517@users.noreply.github.com>
2025-11-19 15:23:53 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LanguageProvider } from './context/LanguageContext';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { About } from './components/About';
@@ -11,19 +12,21 @@ import { Contact } from './components/Contact';
 
 function App() {
   return (
-    <div className="relative min-h-screen bg-slate-50">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Certifications />
-        <Services />
-        <CaseStudies />
-        <Testimonials />
-        <Experience />
-        <Contact />
-      </main>
-    </div>
+    <LanguageProvider>
+      <div className="relative min-h-screen bg-slate-50">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Certifications />
+          <Services />
+          <CaseStudies />
+          <Testimonials />
+          <Experience />
+          <Contact />
+        </main>
+      </div>
+    </LanguageProvider>
   );
 }
 
