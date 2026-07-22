99c38b670e8488e2d5c3c7de4d8f7826c411ed9e
JosepNR97 <josepnunez97@gmail.com>
2026-07-22 07:33:34 +0200

    Certi logo size 2

--- components/Education.tsx
+++ components/Education.tsx
@@ -199,7 +199,7 @@ export const Education = ({ expandedVendorId, onVendorToggle }: EducationProps) 
                                 height="64"
                                 loading="eager"
                                 decoding="sync"
-                                className="h-full w-full object-contain"
+                                className="h-12 w-12 object-contain"
                               />
                             </span>
                             <div className="min-w-0 flex-1">
